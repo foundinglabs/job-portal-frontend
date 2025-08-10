f084c95e0eb3865e2dad8cacc71d82caedee17bf
Rohan <ghadagerohan07@gmail.com>
2025-08-10 15:49:06 +0530

    added redirect url

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -107,7 +107,7 @@ export const AuthProvider = ({ children }) => {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: provider,
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: 'https://job-portal-frontend-iota-eight.vercel.app',
         },
       });
       if (error) throw error;
